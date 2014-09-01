5de73c53d015586aa9a6f2742cfd8b41c1cfd364
cmacro <fpack@163.com>
2014-09-01 22:55:38 +0800

    增加串口通讯

---
+++ Common/serial.h
@@ -0,0 +1,28 @@
+/**
+  ********************************************************************
+  *
+  * @file     serial.c
+  * @author   fpack
+  * @version  v1.0
+  * @date     2014-9-1
+  * @brief    小穆妹纸串口调试
+  *
+  ********************************************************************
+  **/
+
+
+#ifndef __SERIAL_H__
+#define __SERIAL_H__
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
+void serial_init(void);
+
+
+#ifdef __cplusplus    
+  }
+#endif
+
+#endif   
